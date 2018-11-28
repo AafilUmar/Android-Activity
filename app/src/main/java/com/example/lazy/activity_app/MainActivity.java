0c9e304eaf1c19af31ca9aed600c0f6afea75bef
iamAafil <afil.umar007@gmail.com>
2018-11-28 22:17:15 +0530

    two button click and backgound change

--- app/src/main/java/com/example/lazy/activity_app/MainActivity.java
+++ app/src/main/java/com/example/lazy/activity_app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.lazy.activity_app;
 
+import android.graphics.Color;
 import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = MainActivity.class.getSimpleName() ;
@@ -21,11 +23,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         button1=(Button) findViewById(R.id.button);
         button2=(Button) findViewById(R.id.button2);
         relativeLayout=(RelativeLayout)findViewById(R.id.relativelayout);
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View v) {
-        Log.i(TAG,"button clicked");
+        switch(v.getId()){
+
+            case R.id.button:
+                relativeLayout.setBackgroundColor(Color.BLACK);
+                Log.i(TAG,"Black background");
+                break;
+            case R.id.button2:
+                relativeLayout.setBackgroundColor(Color.GREEN);
+                Log.i(TAG,"green background");
+
+                break;
+
+        }
+
     }
 }
