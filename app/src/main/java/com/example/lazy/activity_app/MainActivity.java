5cde6b7e248448414dcce743fb1dcde8c4fad304
iamAafil <afil.umar007@gmail.com>
2018-11-29 20:04:25 +0530

    send data to second activity

--- app/src/main/java/com/example/lazy/activity_app/MainActivity.java
+++ app/src/main/java/com/example/lazy/activity_app/MainActivity.java
@@ -8,12 +8,13 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = MainActivity.class.getSimpleName() ;
     private Button button1;
+    private EditText editText;
 
 
 
@@ -22,12 +23,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button1=(Button) findViewById(R.id.button);
-
+        editText =(EditText)findViewById(R.id.editText);
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent =new Intent(MainActivity.this,Second_Acticity.class);
+
+                Bundle b = new Bundle();
+                b.putString("name",editText.getText().toString());
+                b.putInt("age",23);
+                intent.putExtras(b);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/lazy/activity_app/Second_Acticity.java
+++ app/src/main/java/com/example/lazy/activity_app/Second_Acticity.java
@@ -1,14 +1,33 @@
 package com.example.lazy.activity_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
 
 public class Second_Acticity extends AppCompatActivity {
+
+
+    private static final String TAG = Second_Acticity.class.getSimpleName();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_activity);
+
+        TextView textView =(TextView)findViewById(R.id.textView);
+        StringBuilder msg =new StringBuilder("Welcome\n");
+        Intent intent =getIntent();
+        Bundle b = intent.getExtras();
+         String name = b.getString("name");
+         msg.append(name+"\n");
+
+         int  age =b.getInt("age");
+          msg.append(age);
+          textView.setText(msg);
+
+
     }
 }
